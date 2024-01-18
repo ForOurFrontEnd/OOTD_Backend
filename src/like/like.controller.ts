ad6fd809688b427c3e028175e136e5c8bb27a4e2
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2024-01-18 11:35:36 +0900

    like api completed.

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Headers, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Header, Headers, Post, Res } from '@nestjs/common';
 import { LikeService } from './like.service';
 import { UserService } from 'src/member/user/user.service';
 
@@ -17,7 +17,47 @@ export class LikeController {
         throw new Error('유효한 사용자를 찾을 수 없습니다.');
       }
       const user = await this.userService.findByEmail(decodeData.user.email)
-      const result = await this.likeService.pushLikeButton(user.u_id, 11);
+      const result = await this.likeService.pushLikeButton(user.u_id, 20);
+      res.send(result);
+    } catch (error) {
+      console.error("pressLikeButton Error:", error);
+      res.status(500).send({
+        message: '좋아요 처리 중 오류가 발생했습니다.',
+        success: false,
+        error: error.message || 'Internal Server Error',
+      });
+    }
+  }
+
+  @Get('get_items')
+  async getLikeItems(@Headers('cookie') cookie, @Res() res) {
+    try {
+      const decodeData = await this.userService.decodeToken(cookie);
+      if (!decodeData) {
+        throw new Error('유효한 사용자를 찾을 수 없습니다.');
+      }
+      const user = await this.userService.findByEmail(decodeData.user.email)
+      const result = await this.likeService.findLikeItems(user.u_id);
+      res.send(result);
+    } catch (error) {
+      console.error("pressLikeButton Error:", error);
+      res.status(500).send({
+        message: '좋아요 처리 중 오류가 발생했습니다.',
+        success: false,
+        error: error.message || 'Internal Server Error',
+      });
+    }
+  }
+
+  @Post('unlike_press')
+  async pressUnLikeButton(@Headers('cookie') cookie, @Res() res, @Body('itemId') itemId) {
+    try {
+      const decodeData = await this.userService.decodeToken(cookie);
+      if (!decodeData) {
+        throw new Error('유효한 사용자를 찾을 수 없습니다.');
+      }
+      const user = await this.userService.findByEmail(decodeData.user.email)
+      const result = await this.likeService.pressUnLikeButton(user.u_id, itemId);
       res.send(result);
     } catch (error) {
       console.error("pressLikeButton Error:", error);
