e7d4a5e6dcb7faf3194fa49616be6a3223273749
LKW9 <sg4582@naver.com>
2024-01-29 07:22:13 +0900

    order 기능 구현중

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,14 +1,56 @@
-import { Controller, Delete, Headers, Post, Put, Req, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Headers, Post, Put, Req, Res } from '@nestjs/common';
 import { UserService } from 'src/member/user/user.service';
 import { OrderService } from './order.service';
+import { CartService } from 'src/cart/cart.service';
 
 @Controller('order')
 export class OrderController {
     constructor(
         private readonly userService: UserService,
-        private readonly orderService: OrderService
+        private readonly orderService: OrderService,
+        private readonly cartService: CartService
     ) { }
 
+    @Get("carts")
+    async orderCarts(@Headers("cookie") cookie, @Req() req, @Res() res) {
+        try {
+            const decodeData = await this.userService.decodeToken(cookie);
+            if (!decodeData) {
+                throw new Error('유효한 사용자를 찾을 수 없습니다.');
+            }
+            const { cart } = req.query;
+            const carts = await this.orderService.getCarts(cart)
+            res.send(carts);
+        } catch (error) {
+            console.error("pressLikeButton Error:", error);
+            res.status(500).send({
+                message: '결제 페이지에서 오류가 발생했습니다.',
+                success: false,
+                error: error.message || 'Internal Server Error',
+            });
+        }
+    }
+    @Post('press')
+    async pressLikeButton(@Headers('cookie') cookie, @Res() res, @Body('itemId') itemId: number) {
+        try {
+            const decodeData = await this.userService.decodeToken(cookie);
+            if (!decodeData) {
+                throw new Error('유효한 사용자를 찾을 수 없습니다.');
+            }
+            const user = await this.userService.findByEmail(decodeData.user.email)
+            const result = await this.cartService.pushCartButton(user.u_id, itemId);
+            res.send(result);
+        } catch (error) {
+            console.error("pressLikeButton Error:", error);
+            res.status(500).send({
+                message: '좋아요 처리 중 오류가 발생했습니다.',
+                success: false,
+                error: error.message || 'Internal Server Error',
+            });
+        }
+    }
+
+    //수정 내용 cart[]을 받아서 저장하고 엔티티 수정 cart랑 연결 
     @Post("requst")
     async orderRequst(@Headers("cookie") cookie, @Req() req, @Res() res) {
         try {
@@ -17,6 +59,7 @@ export class OrderController {
                 throw new Error('유효한 사용자를 찾을 수 없습니다.');
             }
             const user = await this.userService.findByEmail(decodeData.user.email)
+            const cart = await this.cartService.getCartData(user.u_id)
             const result = await this.orderService.buyOrder(user.u_id, 20, 3, "s");
             res.send(result);
         } catch (error) {
@@ -29,49 +72,8 @@ export class OrderController {
         }
     }
 
-    // 판매자 배송정보 수정 구현중 미완성
-    // @Put("change")
-    // async sellerOrder(@Headers("cookie") cookie, @Req() req, @Res() res) {
-    //     try {
-    //         const decodeData = await this.userService.decodeToken(cookie);
-    //         if (!decodeData) {
-    //             throw new Error('유효한 사용자를 찾을 수 없습니다.');
-    //         }
-    //         const user = await this.userService.findByEmail(decodeData.user.email)
-    //         const result = await this.orderService.buyOrder(user.u_id, 20, 3, "s");
-    //         res.send(result);
-    //     } catch (error) {
-    //         console.error("pressLikeButton Error:", error);
-    //         res.status(500).send({
-    //             message: '결제 처리 중 오류가 발생했습니다.',
-    //             success: false,
-    //             error: error.message || 'Internal Server Error',
-    //         });
-    //     }
-    // }
-
     @Delete("cancel")
     async orderCancel(@Headers("cookie") cookie, @Req() req, @Res() res) {
-        try {
-            const decodeData = await this.userService.decodeToken(cookie);
-            if (!decodeData) {
-                throw new Error('유효한 사용자를 찾을 수 없습니다.');
-            }
-            const orderid = req.body.oderid
-            const result = await this.orderService.cancelOrder(orderid);
-            res.send(result);
-        } catch (error) {
-            console.error("pressLikeButton Error:", error);
-            res.status(500).send({
-                message: '결제 처리 중 오류가 발생했습니다.',
-                success: false,
-                error: error.message || 'Internal Server Error',
-            });
-        }
-    }
-
-    @Delete("allcancel")
-    async allOrderCancel(@Headers("cookie") cookie, @Req() req, @Res() res) {
         try {
             const decodeData = await this.userService.decodeToken(cookie);
             if (!decodeData) {
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -4,6 +4,7 @@ import { Order } from './entity/order.entity';
 import { Repository } from 'typeorm';
 import { User } from 'src/member/user/entity/user.entity';
 import { Item } from 'src/item/entity/item.entity';
+import { Cart } from 'src/cart/entity/cart.entity';
 
 @Injectable()
 export class OrderService {
@@ -14,6 +15,8 @@ export class OrderService {
         private userRepository: Repository<User>,
         @InjectRepository(Item)
         private itemRepository: Repository<Item>,
+        @InjectRepository(Cart)
+        private cartRepository: Repository<Cart>,
     ) { }
 
     async buyOrder(userId: string, itemId: number, quantity: number, size: string): Promise<any> {
@@ -36,7 +39,7 @@ export class OrderService {
             return {
                 message: '구매 요청이 성공적으로 저장되었습니다.',
                 success: true,
-                like: savedOrder,
+                order: savedOrder,
             };
         } catch (error) {
             console.error("buyOrder Error:", error);
@@ -57,17 +60,65 @@ export class OrderService {
                     message: '주문이 성공적으로 취소되었습니다.',
                     success: true,
                 }
-            }else {
+            } else {
                 return {
                     message: '상품이 배송중이라 취소할수없습니다.',
                     success: false,
                 }
             }
-            
+
         } catch (error) {
             console.error("cancelOrder Error:", error);
             throw new Error('주문 취소 중 오류가 발생했습니다.');
         }
     }
 
+    async getCarts(carts: number[]) {
+        const cartArray = [];
+        await Promise.all(carts.map(async (item) => {
+            const cart = await this.cartRepository.findOne({ where: { c_id: item }, relations: ['item'], });
+            cartArray.push(cart);
+        }));
+        return cartArray
+    }
+
+    async pushCartButton(userId: string, itemId: number): Promise<any> {
+        try {
+            const user = await this.userRepository.findOne({ where: { u_id: userId } });
+            const item = await this.itemRepository.findOne({ where: { i_id: itemId } });
+    
+            if (!user || !item) {
+                throw new Error('사용자 또는 아이템을 찾을 수 없습니다.');
+            }
+    
+            const existingLike = await this.cartRepository.findOne({ where: { user: { u_id: userId }, item: { i_id: itemId } } });
+            console.log("1",existingLike);
+            
+            if (existingLike) {
+                await this.cartRepository.remove(existingLike);
+                return {
+                    message: '이미 들어가있는 장바구니',
+                    success: true,
+                    like: existingLike,
+                };
+            }
+    
+            const newCart = new Cart();
+            newCart.user = user;
+            newCart.item = item;
+    
+            const savedCart = await this.cartRepository.save(newCart);
+            console.log("2",savedCart);
+            
+            return {
+                message: '좋아요가 성공적으로 저장되었습니다.',
+                success: true,
+                like: savedCart,
+            };
+        } catch (error) {
+            console.error("pushLikeButton Error:", error);
+            throw new Error('좋아요 저장 중 오류가 발생했습니다.');
+        }
+    }
+    
 }
